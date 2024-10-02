4d1c01bd2b3017b93007bf8cfc076090be99773f
wodms2ekd@ <jei000215@gmail.com>
2024-10-02 15:40:07 +0900

    1

---
+++ test01/test01/main.c
@@ -0,0 +1,18 @@
+/*
+ * test01.c
+ *
+ * Created: 2024-10-02 오후 1:24:38
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
